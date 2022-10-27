11493a4f633ac4f27be825aeae92629ba8590f2e
Jessica Mosby <jessicamosby@mac.com>
2022-10-27 18:54:03 -0400

    added answer options & function section in js

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-/* Get DOM Elements */
 const submitBtn = document.getElementById('submit');
 const resetBtn = document.getElementById('reset');
 const promptSection = document.getElementById('prompt');
 const fortuneSection = document.getElementById('fortune');
 const answerP = document.getElementById('results');
 
-/* State */
+const answers = [
+    'Yes, definitely',
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most Likely',
+    'Outlook good',
+    'Signs point to yes',
+    'Reply hazy, try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don’t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+];
 
-/* Events */
-
-/* Display Functions */
+function toggleSections() {
+    promptSection.classList.toggle('hide');
+    promptSection.classList.toggle('hide');
+}
 
 // (don't forget to call any display functions you want to run on page load!)
