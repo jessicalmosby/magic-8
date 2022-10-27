22d4be64079de09bea985cd544dde589ff141127
Jessica Mosby <jessicamosby@mac.com>
2022-10-27 18:40:36 -0400

    fixed sintax error in html & added const to js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-/* Imports */
-
 /* Get DOM Elements */
+const submitBtn = document.getElementById('submit');
+const resetBtn = document.getElementById('reset');
+const promptSection = document.getElementById('prompt');
+const fortuneSection = document.getElementById('fortune');
+const answerP = document.getElementById('results');
 
 /* State */
 
